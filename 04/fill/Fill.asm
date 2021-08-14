c8dcc6dca8f3a5315b00cce4e7720de013423626
Ron mac <ronkrasniqi_@hotmail.co.uk>
2021-08-15 00:48:51 +0200

    WIP

--- 04/fill/Fill.asm
+++ 04/fill/Fill.asm
@@ -12,3 +12,46 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+// Solution
+// 1. Loop listening for keyboard input
+// 2. If input not pressed, white screen
+// 3. Else draw pixels
+
+//Listen for keyboard
+(LOOP)
+@KBD
+D=A
+@LOOP
+//If not pressed, restart loop
+D;JEQ
+
+(PAINT)
+@SCREEN
+D=A
+@R1
+//Load Screen address into R1
+M=D
+@8192
+D=A
+@PixelAddress
+//Add screen addresses
+M=M+D
+
+
+@65535
+D=A
+
+@PixelAddress
+A=M
+M=D
+
+@PixelAddress
+M=M-1
+
+@SCREEN
+D=D-A
+
+@PAINT
+D;JGE
+@LOOP
+0;JMP

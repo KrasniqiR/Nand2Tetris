3899635e587716cc8d4b95b7d5199cf346290010
KrasniqiR <ron.krasniqi@outlook.com>
2022-01-08 13:36:49 +0000

    Improve patterns

--- 06/assembler/src/patterns.test.ts
+++ 06/assembler/src/patterns.test.ts
@@ -1,9 +1,19 @@
 import {assertObjectMatch, assertExists} from '../deps.ts';
 import { cInstruction } from "./patterns.ts";
 
-Deno.test("Parses a C instruction", () => {
-    const instruction = "AM=M+D;JGE"
-    const result = instruction.match(cInstruction);
-    assertExists(result?.groups);
-    assertObjectMatch(result.groups, {dest: 'AM', comp: 'M+D', jump: 'JGE'});
+Deno.test("Parses a C instructions", () => {
+    const instructionsResultsMap: Array<[string,  any]> = [
+        ["AM=M+D;JGE", {dest: 'AM', comp: 'M+D', jump: 'JGE'}],
+        // ["0", {comp: "0"}],
+    ];
+        
+    
+    instructionsResultsMap.forEach(([instruction, expectedResult]) => {
+        console.log(instruction, expectedResult);
+        const result = instruction.match(cInstruction);
+        console.log(result);
+        assertExists(result?.groups);
+        assertObjectMatch(result.groups, expectedResult);
+    });
+
 })

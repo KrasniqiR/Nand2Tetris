6576758b8dfe19df27f9a12f9ddcce5dc980150b
KrasniqiR <ron.krasniqi@outlook.com>
2022-01-13 20:57:04 +0000

    fmt

--- 06/assembler/src/patterns.test.ts
+++ 06/assembler/src/patterns.test.ts
@@ -1,19 +1,17 @@
-import {assertObjectMatch, assertExists} from '../deps.ts';
+import { assertExists, assertObjectMatch } from "../deps.ts";
 import { cInstruction } from "./patterns.ts";
 
 Deno.test("Parses a C instructions", () => {
-    const instructionsResultsMap: Array<[string,  any]> = [
-        ["AM=M+D;JGE", {dest: 'AM', comp: 'M+D', jump: 'JGE'}],
-        // ["0", {comp: "0"}],
-    ];
-        
-    
-    instructionsResultsMap.forEach(([instruction, expectedResult]) => {
-        console.log(instruction, expectedResult);
-        const result = instruction.match(cInstruction);
-        console.log(result);
-        assertExists(result?.groups);
-        assertObjectMatch(result.groups, expectedResult);
-    });
+  const instructionsResultsMap: Array<[string, any]> = [
+    ["AM=M+D;JGE", { dest: "AM", comp: "M+D", jump: "JGE" }],
+    // ["0", {comp: "0"}],
+  ];
 
-})
+  instructionsResultsMap.forEach(([instruction, expectedResult]) => {
+    console.log(instruction, expectedResult);
+    const result = instruction.match(cInstruction);
+    console.log(result);
+    assertExists(result?.groups);
+    assertObjectMatch(result.groups, expectedResult);
+  });
+});
